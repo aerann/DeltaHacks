c0f0c384be247f9a1ff7c126e66a35184e71df25
faith yuchi <yuchi.faith@gmail.com>
2023-01-14 14:53:49 -0500

    finished tutorial video for posture tracking

--- 1.py
+++ 1.py
@@ -13,3 +13,17 @@ with mp_pose.Pose(min_detection_confidemce=0.5,min_tracking_confidence=0.5) as p
         results=pose.process(image)
 
         #drawing indication lines
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        mp_drawing.draw_landmarks(
+            image,
+            results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
+        )
+
+        #flip image horizontally for selfie view
+        cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFFF == 27:
+            break
+    cap.release()
