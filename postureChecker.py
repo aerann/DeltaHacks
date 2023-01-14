a917835b5269e35c7823376d4c3ecab6490fff36
Ann Shi <annshi204a@gmail.com>
2023-01-14 16:10:11 -0500

    added ability to close webcam

--- postureChecker.py
+++ postureChecker.py
@@ -5,12 +5,13 @@ mp_drawing_styles=mp.solutions.drawing_styles
 mp_pose=mp.solutions.pose
 
 cap=cv2.VideoCapture(0)
-with mp_pose.Pose(min_detection_confidemce=0.5,min_tracking_confidence=0.5) as pose:
+
+with mp_pose.Pose(min_detection_confidence = 0.5,min_tracking_confidence = 0.5) as pose:
     while cap.isOpened():
-        success, image=cap.read()
+        success, image = cap.read()
         image.flags.writeable = False
-        image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-        results=pose.process(image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = pose.process(image)
 
         #drawing indication lines
         image.flags.writeable = True
@@ -22,8 +23,12 @@ with mp_pose.Pose(min_detection_confidemce=0.5,min_tracking_confidence=0.5) as p
             landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
         )
 
-        #flip image horizontally for selfie view
+        #flip image horizontally for mirrored view
         cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
-        if cv2.waitKey(5) & 0xFFF == 27:
+
+        #stops running webcam until 'q' is clicked 
+        if cv2.waitKey(1) == ord('q'):
             break
+
     cap.release()
+    cv2.destroyAllWindows()        
