90236adfd49aab65333ebbe368c0fb81b8a403d7
Tathagata Sikdar <rikisikdar1@gmail.com>
2023-01-15 01:31:57 -0500

    did class stuff to test and redid backend

--- backend.py
+++ backend.py
@@ -1,23 +1,22 @@
 from flask import Flask, render_template, Response
-import postureChecker
+import test import VideoCamera
 import cv2
 
 website = Flask(__name__)
 
 @website.route("/")
 def page():
-  return render_template('index.html',barValue = main())
+  return render_template('index.html')
 
-def runCamera():
-  
-    frame = getWebFrame(cap)
-    yield (b'--frame\r\n'
-    b'Content-Type: image/jpeg\r\n\r\n' + frame
-    + b'\r\n\r\n')
+def gen():
+    # frame = getWebFrame(cap)
+    # yield (b'--frame\r\n'
+    # b'Content-Type: image/jpeg\r\n\r\n' + frame
+    # + b'\r\n\r\n')
 
 @website.route('/video_feed')
 def video_feed():
-  return Response(runCamera())
+  return Response(gen(VideoCamera()))
 
 if __name__ == '__main__':
-  website.run(debug=True)
+  website.run(host='0.0.0.0', port='5000', debug=True)
--- test.py
+++ test.py
@@ -1,106 +1,89 @@
 import cv2
 import mediapipe as mp
-from flask import Flask, render_template, Response
 
-# variables made for convenience
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_pose = mp.solutions.pose
-cap = cv2.VideoCapture(0)
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_drawing_styles = mp.solutions.drawing_styles
+        self.mp_pose = mp.solutions.pose
+        self.pose = self.mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5)
+        self.height = self.video.get(4)
+        self.good_left = 0
+        self.good_right = 0
+    def __del__(self):
+        self.video.releast()
+    
+    def get_frame(self):
+        frame = self.video.read()
+        jpeg = cv2.imdecode('.jpg', frame)
 
+    def calculatePercentage(self, l_shldr_y, r_shldr_y,):
+        range = self.height - self.good_left
+        relative = l_shldr_y - self.good_left
+        percentage = 0
+        # if(self.good_left == 0 and self.good_right == 0): 
+        #     print("Press P to record your position, or q to quit!")
+        if(l_shldr_y > self.good_left + 15 or r_shldr_y > self.good_right + 15):
+            percentage = 100- ((relative/range) * 100)
+            if (percentage <= 0):
+                percentage = 0
+            print("BAD POSTURE, %", percentage)
+        else:
+            percentage = 100
+            print("GOOD POSTURE!, %", percentage)
+        return percentage
 
-pose = mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5)
-# while webcam is running
+    def processPosture(self):
+        while self.cap.isOpened():
+            success, image = self.cap.read()
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = self.pose.process(image)
 
-good_left = 0
-good_right = 0
-
-#returns the percentage of how good your posture is 
-def checkPosture(l_shldr_y, r_shldr_y):
-    height = cap.get(4)  # float `height`
-    range = height - good_left
-    relative = l_shldr_y - good_left
-    percentage = 0
-
-    if(good_left == 0 and good_right == 0): 
-        print("Press P to record your position, or q to quit!")
-    elif(l_shldr_y > good_left + 15 or r_shldr_y > good_right + 15):
-        percentage = 100- ((relative/range) * 100)
-        if (percentage <= 0):
-            percentage = 0
-        print("BAD POSTURE, %", percentage)
-    else:
-        percentage = 100
-        print("GOOD POSTURE!, %", percentage)
-    return percentage
-
-def main():
-    success, image = cap.read()
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
-
-    #drawing indication lines
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
-    )
-
-
-    # Use lm and lmPose as representative of the following methods.
-    # Process the image.
-    keypoints = pose.process(image)
-    lm = keypoints.pose_landmarks
-    lmPose = mp_pose.PoseLandmark
-    h,w = image.shape[:2]
+            #drawing indication lines
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            self.mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                self.mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=self.mp_drawing_styles.get_default_pose_landmarks_style()
+            )
 
 
-    #flip image horizontally for mirrored view
-    cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+            # Use lm and lmPose as representative of the following methods.
+            # Process the image.
+            keypoints = self.pose.process(image)
+            lm = keypoints.pose_landmarks
+            lmPose = self.mp_pose.PoseLandmark
+            h,w = image.shape[:2]
 
-    # wait for user to type in p 
-    if cv2.waitKey(1) == ord('p'):
-        print('POSITION RECORDED!')
-        good_left = int(lm.landmark[lmPose.LEFT_SHOULDER].y * h) 
-        good_right = int(lm.landmark[lmPose.RIGHT_SHOULDER].y * h)
 
-    l_shldr_y = int(lm.landmark[lmPose.LEFT_SHOULDER].y * h)
-    r_shldr_y = int(lm.landmark[lmPose.RIGHT_SHOULDER].y * h)
+            #flip image horizontally for mirrored view
+            cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
 
-    checkPercentage = checkPosture(l_shldr_y, r_shldr_y) 
-    return checkPercentage
+            # wait for user to type in p 
+            if cv2.waitKey(1) == ord('p'):
+                print('POSITION RECORDED!')
+                good_left = int(lm.landmark[lmPose.LEFT_SHOULDER].y * h) 
+                good_right = int(lm.landmark[lmPose.RIGHT_SHOULDER].y * h)
 
-    #stops running webcam until 'q' is clicked 
-    if cv2.waitKey(1) == ord('q'):
-        cap.release()
-        cv2.destroyAllWindows()        
+            l_shldr_y = int(lm.landmark[lmPose.LEFT_SHOULDER].y * h)
+            r_shldr_y = int(lm.landmark[lmPose.RIGHT_SHOULDER].y * h)
 
-#----------------------
-def getWebFrame(cam):
-    frame = cam.read()
-    jpeg = cv2.imencode('.jpg', frame)
-    return jpeg.tobytes()
+            checkPercentage = self.checkPosture(l_shldr_y, r_shldr_y) 
+    
 
-website = Flask(__name__)
+    # variables made for convenience
+    # mp_drawing = mp.solutions.drawing_utils
+    # mp_drawing_styles = mp.solutions.drawing_styles
+    # mp_pose = mp.solutions.pose
+    # cap = cv2.VideoCapture(0)
 
-@website.route("/")
-def page():
-    return render_template('index.html')
 
-def runCamera():
-  
-    frame = getWebFrame(cap)
-    yield (b'--frame\r\n'
-    b'Content-Type: image/jpeg\r\n\r\n' + frame
-    + b'\r\n\r\n')
+    # pose = mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5)
+    # while webcam is running
 
-@website.route('/video_feed')
-def video_feed():
-  return Response(runCamera())
 
-if __name__ == '__main__':
-  website.run(debug=True)
+    #returns the percentage of how good your posture is 
